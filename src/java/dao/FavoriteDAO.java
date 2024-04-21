8fd68e61ed096d5d62a6d01ddad209cb0c2f6f0e
sezerd <99814610+sezerdemir7@users.noreply.github.com>
2024-04-21 19:40:11 +0300

    favorileri listeleme işlemleri

--- src/java/dao/FavoriteDAO.java
+++ src/java/dao/FavoriteDAO.java
@@ -5,6 +5,10 @@
 package dao;
 
 import entity.Favorite;
+import entity.Product;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import util.DBConnect;
 
@@ -12,11 +16,47 @@ import util.DBConnect;
  *
  * @author serki
  */
-public class FavoriteDAO extends DBConnect implements BaseDAO<Favorite>{
+public class FavoriteDAO extends DBConnect implements BaseDAO<Favorite> {
+    
+    private ProductDAO productDAO;
+
+    // Constructor Injection
+    public FavoriteDAO(ProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
 
     @Override
-    public void create(Favorite entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void create(Favorite favorite) {
+        try {
+
+            Statement st = this.getConnect().createStatement();
+            st.executeUpdate("insert into favorite(product_id,customer_id) values ('" + favorite.getProduct().getId() + "','" + favorite.getCustomer().getId() + "')");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Product> getFavoriteListByCustomerId(Long customerId) {
+        List<Long> productList = new ArrayList<>();
+        List<Product> productObjectList = new ArrayList<>();
+        try {
+            Statement st = this.getConnect().createStatement();
+            ResultSet rs = st.executeQuery("select * from favorite");
+
+            while (rs.next()) {
+
+                productList.add(rs.getLong("product_id"));
+
+            }
+
+        } catch (Exception e) {
+
+            System.out.println("Error while reading product list: " + e.getMessage());
+        }
+        
+        //productDAO.getProductListByProductId(productList);
+        
+        return productDAO.readList();
     }
 
     @Override
@@ -38,5 +78,5 @@ public class FavoriteDAO extends DBConnect implements BaseDAO<Favorite>{
     public Favorite getEntityById(Long id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
